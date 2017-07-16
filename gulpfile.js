15e85cabd5b3bfb2b618aeaa85947c8965110549
Ben Hong <bencodezen@gmail.com>
2017-07-16 19:17:34 -0400

    config: Change directory for sass files

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var browserSync = require('browser-sync')
 
 gulp.task('sass', function() {
 	return gulp
-		.src('./src/sass/**/*.scss')
+		.src('./src/scss/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./src/'))
 })
@@ -16,8 +16,7 @@ gulp.task('serve', function() {
 		}
 	})
 
-	gulp.watch('src/sass/*.scss', ['sass'])
-
+	gulp.watch('src/scss/*.scss', ['sass'])
 	gulp.watch(['*.html', '*.css'], { cwd: 'src' }, browserSync.reload)
 })
 
