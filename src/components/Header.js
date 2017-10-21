39634a0971123fd0cbb46debe14cc851821c3bc3
Ben Hong <bencodezen@gmail.com>
2017-10-21 11:20:18 -0700

    enhance (#4): Update Header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
 
 const Header = props => {
 	return (
-		<header>
-			<h1>BenCodeZen</h1>
-		</header>
+		<HeaderContainer>
+			<Logo>
+				<Link to="/">BenCodeZen</Link>
+			</Logo>
+		</HeaderContainer>
 	)
 }
+
+const HeaderContainer = styled.header`
+	display: flex;
+	padding: 1.5rem;
+`
+
+const Logo = styled.h1`margin: 0;`
 
 export default Header
