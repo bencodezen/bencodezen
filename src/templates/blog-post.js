92736c068be328d957967d23c3fa380fe4feb837
Ben Hong <bencodezen@gmail.com>
2017-10-21 11:20:04 -0700

    enhance (#10): Add navigation to article page

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 
 export default ({ data }) => {
 	const post = data.markdownRemark
 	return (
-		<div style={{ margin: '3rem auto', maxWidth: 600 }}>
-			<h1>{post.frontmatter.title}</h1>
-			<div dangerouslySetInnerHTML={{ __html: post.html }} />
+		<div>
+			<Header />
+			<article style={{ margin: '3rem auto', maxWidth: 600 }}>
+				<h1>{post.frontmatter.title}</h1>
+				<div dangerouslySetInnerHTML={{ __html: post.html }} />
+			</article>
+			<Footer />
 		</div>
 	)
 }
