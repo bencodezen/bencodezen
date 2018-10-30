4827f7245f14e667ae144e17fe2057b989c54004
Ben Hong <bencodezen@gmail.com>
2018-10-29 20:48:03 -0400

    feature: Configure basic heading with navigation links

---
+++ src/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	title: 'BenCodeZen',
+	themeConfig: {
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'Twitter', link: 'https://www.twitter.com/bencodezen' },
+			{ text: 'GitLab', link: 'https://www.gitlab.com/bencodezen' }
+		]
+	}
+}
