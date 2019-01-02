0ee0e4441e75e5ddabe961ae1535d4acfee3b7bf
Ben Hong <bencodezen@gmail.com>
2019-01-01 20:57:03 -0500

    feature: Update typography pairing

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -30,6 +30,7 @@ module.exports = {
 		]
 	],
 	head: [
+		['link', { 'rel': "stylesheet", 'href': "https://fonts.googleapis.com/css?family=Karla:700|Lato:400,400i,700" }],
 		['link', { 'rel': "apple-touch-icon", 'sizes': "57x57", 'href': "/apple-icon-57x57.png" }],
 		['link', { 'rel': "apple-touch-icon", 'sizes': "60x60", 'href': "/apple-icon-60x60.png" }],
 		['link', { 'rel': "apple-touch-icon", 'sizes': "72x72", 'href': "/apple-icon-72x72.png" }],
