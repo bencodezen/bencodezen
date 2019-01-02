9b8a16d580b7b88dd8e640a0b4efce4578e77fe9
Ben Hong <bencodezen@gmail.com>
2019-01-01 21:28:21 -0500

    refactor: Organize nav into more condensed items

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -11,10 +11,15 @@ module.exports = {
 		editLinkText: 'Found a bug? Help me improve this page!',
 		nav: [
 			{ text: 'Home', link: '/' },
-			{ text: 'Learn', link: '/learn/'},
-			{ text: 'Uses', link: '/uses/'},
-			{ text: 'About', link: '/about/' },
-			{ text: 'Contact', link: '/contact/' },
+			{ text: 'Learn', link: '/learn/' },
+			{
+				text: 'About',
+				items: [
+					{ text: 'Intro', link: '/about/' },
+					{ text: 'Uses', link: '/uses/' },
+					{ text: 'Contact', link: '/contact/' }
+				]
+			},
 			{ text: 'RSS Feed', link: '/rss.xml' }
 		]
 	},
@@ -24,29 +29,118 @@ module.exports = {
 			{
 				base_url: '/',
 				site_url: 'https://www.bencodezen.io',
-				filter: (frontmatter => frontmatter.date <= new Date()),
+				filter: frontmatter => frontmatter.date <= new Date(),
 				count: 20
 			}
 		]
 	],
 	head: [
-		['link', { 'rel': "stylesheet", 'href': "https://fonts.googleapis.com/css?family=Karla:700|Lato:400,400i,700" }],
-		['link', { 'rel': "apple-touch-icon", 'sizes': "57x57", 'href': "/apple-icon-57x57.png" }],
-		['link', { 'rel': "apple-touch-icon", 'sizes': "60x60", 'href': "/apple-icon-60x60.png" }],
-		['link', { 'rel': "apple-touch-icon", 'sizes': "72x72", 'href': "/apple-icon-72x72.png" }],
-		['link', { 'rel': "apple-touch-icon", 'sizes': "76x76", 'href': "/apple-icon-76x76.png" }],
-		['link', { 'rel': "apple-touch-icon", 'sizes': "114x114", 'href': "/apple-icon-114x114.png" }],
-		['link', { 'rel': "apple-touch-icon", 'sizes': "120x120", 'href': "/apple-icon-120x120.png" }],
-		['link', { 'rel': "apple-touch-icon", 'sizes': "144x144", 'href': "/apple-icon-144x144.png" }],
-		['link', { 'rel': "apple-touch-icon", 'sizes': "152x152", 'href': "/apple-icon-152x152.png" }],
-		['link', { 'rel': "apple-touch-icon", 'sizes': "180x180", 'href': "/apple-icon-180x180.png" }],
-		['link', { 'rel': "icon", 'type': "image/png", 'sizes': "192x192", 'href': "/android-icon-192x192.png" }],
-		['link', { 'rel': "icon", 'type': "image/png", 'sizes': "32x32", 'href': "/favicon-32x32.png" }],
-		['link', { 'rel': "icon", 'type': "image/png", 'sizes': "96x96", 'href': "/favicon-96x96.png" }],
-		['link', { 'rel': "icon", 'type': "image/png", 'sizes': "16x16", 'href': "/favicon-16x16.png" }],
-		['link', { 'rel': "manifest", 'href': "/manifest.json" }],
-		['meta', { 'name': "msapplication-TileColor", 'content': "#ffffff" }],
-		['meta', { 'name': "msapplication-TileImage", 'content': "/ms-icon-144x144.png" }],
-		['meta', { 'name': "theme-color", 'content': "#ffffff" }]
+		[
+			'link',
+			{
+				rel: 'stylesheet',
+				href:
+					'https://fonts.googleapis.com/css?family=Karla:700|Lato:400,400i,700'
+			}
+		],
+		[
+			'link',
+			{ rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-icon-57x57.png' }
+		],
+		[
+			'link',
+			{ rel: 'apple-touch-icon', sizes: '60x60', href: '/apple-icon-60x60.png' }
+		],
+		[
+			'link',
+			{ rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-icon-72x72.png' }
+		],
+		[
+			'link',
+			{ rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-icon-76x76.png' }
+		],
+		[
+			'link',
+			{
+				rel: 'apple-touch-icon',
+				sizes: '114x114',
+				href: '/apple-icon-114x114.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'apple-touch-icon',
+				sizes: '120x120',
+				href: '/apple-icon-120x120.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'apple-touch-icon',
+				sizes: '144x144',
+				href: '/apple-icon-144x144.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'apple-touch-icon',
+				sizes: '152x152',
+				href: '/apple-icon-152x152.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'apple-touch-icon',
+				sizes: '180x180',
+				href: '/apple-icon-180x180.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '192x192',
+				href: '/android-icon-192x192.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '32x32',
+				href: '/favicon-32x32.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '96x96',
+				href: '/favicon-96x96.png'
+			}
+		],
+		[
+			'link',
+			{
+				rel: 'icon',
+				type: 'image/png',
+				sizes: '16x16',
+				href: '/favicon-16x16.png'
+			}
+		],
+		['link', { rel: 'manifest', href: '/manifest.json' }],
+		['meta', { name: 'msapplication-TileColor', content: '#ffffff' }],
+		[
+			'meta',
+			{ name: 'msapplication-TileImage', content: '/ms-icon-144x144.png' }
+		],
+		['meta', { name: 'theme-color', content: '#ffffff' }]
 	]
 }
