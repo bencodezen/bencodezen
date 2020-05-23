858c606c6cedd8e6b68abd3c4c83dd7cc152b9ad
Ben Hong <ben@bencodezen.io>
2020-05-23 07:57:53 -0400

    config: add tailwindcss

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,8 @@ export default {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module'
+    '@nuxtjs/stylelint-module',
+    '@nuxtjs/tailwindcss'
   ],
   /*
    ** Nuxt.js modules
