d4978ee0ec20ec43f1c06e54425da9a2bdefd56f
Ben Hong <ben@bencodezen.io>
2020-11-30 20:28:54 -0500

    feature: add styles for layout and background

--- pages/learn/dev-advent-2020.vue
+++ pages/learn/dev-advent-2020.vue
@@ -15,24 +15,48 @@ export default {
 </script>
 
 <template>
-  <main class="dev-advent-wrapper">
-    <h1>DevAdvent Calendar</h1>
-    <p>
-      December is going to be an exciting month! Come back each day before
-      Christmas to see a new video, livestream, and/or talk that'll go live!
-    </p>
-    <ul>
-      <li v-for="day in days" :key="`day-${day}`">
-        {{ day }}
-        <img src="../../assets/images/dev-advent-2020/lights.svg" />
-      </li>
-    </ul>
+  <main class="dev-advent-page">
+    <article class="dev-advent-wrapper">
+      <h1 class="daw-title">DevAdvent Calendar</h1>
+      <p class="daw-description">
+        December is going to be an exciting month! Come back each day before
+        Christmas to see a new video, livestream, and/or talk that'll go live!
+      </p>
+      <ul>
+        <li v-for="day in days" :key="`day-${day}`">
+          {{ day }}
+          <img src="../../assets/images/dev-advent-2020/lights.svg" />
+        </li>
+      </ul>
+    </article>
   </main>
 </template>
 
 <style>
+.dev-advent-page {
+  background-color: #0a4846;
+
+  background-image: url('../../assets/images/dev-advent-2020/bg-snowflake.png');
+}
+
 .dev-advent-wrapper {
-  width: 766px;
+  max-width: 766px;
   margin: 0 auto;
+  color: #fcfcfc;
+  text-align: center;
+}
+
+.daw-title {
+  text-align: center;
+  font-size: 40px;
+  margin-top: 60px;
+  margin-bottom: 20px;
+  padding-bottom: 15px;
+  border-bottom: 2px solid #fcfcfc;
+  display: inline-block;
+}
+
+.daw-description {
+  max-width: 700px;
 }
 </style>
