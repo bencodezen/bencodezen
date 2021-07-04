f3693b00d5128d9b516cd8b483c1b7f2d98618ef
Ben Hong <ben@bencodezen.io>
2021-07-04 17:04:52 -0400

    feature: add wip schedule page

---
+++ pages/schedule.vue
@@ -0,0 +1,34 @@
+<script>
+export default {}
+</script>
+
+<template>
+  <main class="container mx-auto content">
+    <div class="hero">
+      <h1>Schedule</h1>
+      <p>Here's what's coming up on the schedule!</p>
+      <p>
+        Make sure to never miss an episode by adding the Google Calendar,
+        bookmarking YouTube, and/or subscribing on Twitch!
+      </p>
+      <a href="/calendar" target="_blank">Add to Calendar</a>
+      <a href="https://www.youtube.com/bencodezen">Subscribe on YouTube</a>
+      <a href="https://www.twitch.tv/bencodezen">Subscribe on Twitch</a>
+    </div>
+    <h2>Upcoming</h2>
+    <ul>
+      <li></li>
+    </ul>
+    <h2>Past Episodes</h2>
+  </main>
+</template>
+
+<style>
+.hero {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 4rem 5vw;
+}
+</style>
