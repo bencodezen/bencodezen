a28e9bffaa222991b4a7364d3df92d730955804d
Ben Hong <ben@bencodezen.io>
2021-07-05 23:41:38 -0400

    config: remove eslint for v-slot

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,14 @@ module.exports = {
   ],
   plugins: ['prettier'],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'vue/v-slot-style': [
+      'error',
+      {
+        atComponent: 'v-slot',
+        default: 'longform',
+        named: 'longform'
+      }
+    ]
+  }
 }
