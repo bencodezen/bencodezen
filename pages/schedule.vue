4d2e00feb194801deeb6960cedf8ef7118a274e4
Ben Hong <ben@bencodezen.io>
2021-07-05 23:05:19 -0400

    feature: update schedule styles

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -10,16 +10,19 @@ export default {}
         Here's what's coming up on the schedule! You can add the schedule to
         your calendar and/or subscribe on Twitch!
       </p>
-      <a
-        class="btn"
-        href="https://calendar.google.com/calendar/u/0?cid=Y19ydWlzbHQzcnVsZjh0MGQ1aXU1OGpibGs3MEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t"
-        target="_blank"
-        >Add to Calendar</a
-      >
+      <div class="schedule-cta">
+        <a
+          class="btn"
+          href="https://calendar.google.com/calendar/u/0?cid=Y19ydWlzbHQzcnVsZjh0MGQ1aXU1OGpibGs3MEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t"
+          target="_blank"
+        >
+          Add to Calendar
+        </a>
 
-      <a class="btn" href="https://www.twitch.tv/bencodezen">
-        Subscribe on Twitch
-      </a>
+        <a class="btn" href="https://www.twitch.tv/bencodezen">
+          Subscribe on Twitch
+        </a>
+      </div>
       <h2>Upcoming</h2>
       <div class="schedule-item">
         <h3 class="title">Build with Ben (#7)</h3>
@@ -34,18 +37,29 @@ export default {}
 </template>
 
 <style lang="scss">
-a.btn {
-  border: 2px solid #22aaff;
-  text-decoration: none;
-  padding: 1rem;
-  border-radius: 8px;
-  color: #22aaff;
-  font-weight: 600;
-}
-
 .schedule-item {
   .title {
     margin-bottom: 0;
+  }
+}
+
+.schedule-cta {
+  display: flex;
+  justify-content: center;
+
+  .btn {
+    margin-right: 2rem;
+    border: 2px solid #22aaff;
+    text-decoration: none;
+    padding: 1rem;
+    border-radius: 8px;
+    color: #22aaff;
+    font-weight: 600;
+    white-space: nowrap;
+  }
+
+  .btn:last-child {
+    margin-right: 0;
   }
 }
 </style>
