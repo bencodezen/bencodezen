7681a2ff484c22b7abd4c6d205f7b96e737988fa
Ben Hong <ben@bencodezen.io>
2022-01-17 11:25:45 -0500

    feature: archive event

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -68,19 +68,6 @@ export default {
       <ul>
         <li class="schedule-item">
           <h3 class="title">
-            Build with Ben (#24): Trello Clone with Nuxt 3 (Beta) (Part 2)
-          </h3>
-          <p class="date">
-            Tuesday, Jan 11th, 2022 from 12:00PM to 1:30PM EST
-          </p>
-          <p class="location">
-            <a href="https://www.twitch.tv/bencodezen"
-              >https://www.twitch.tv/bencodezen</a
-            >
-          </p>
-        </li>
-        <li class="schedule-item">
-          <h3 class="title">
             Productivity Power Hour (#2): Let's Talk About with RemNote
           </h3>
           <p class="date">Thursday, Jan 13th, 2022 from 2:00PM to 3:00PM EST</p>
@@ -143,6 +130,19 @@ export default {
       </ul>
       <h2>Past</h2>
       <ul>
+        <li class="schedule-item">
+          <h3 class="title">
+            Build with Ben (#24): Trello Clone with Nuxt 3 (Beta) (Part 2)
+          </h3>
+          <p class="date">
+            Tuesday, Jan 11th, 2022 from 12:00PM to 1:30PM EST
+          </p>
+          <p class="location">
+            <a href="https://www.twitch.tv/bencodezen"
+              >https://www.twitch.tv/bencodezen</a
+            >
+          </p>
+        </li>
         <li class="schedule-item">
           <h3 class="title">
             Productivity Power Hour (#1): My Workflow for Making Notes in
