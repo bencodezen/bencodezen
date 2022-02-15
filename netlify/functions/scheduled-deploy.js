ce27435b1e17852100df4a39752a2528497d97d1
Ben Hong <ben@bencodezen.io>
2022-02-15 11:28:58 -0500

    feature: add ability to deploy from scheduled function

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -1,11 +1,18 @@
+const fetch = require('node-fetch')
 const { schedule } = require('@netlify/functions')
 
-const handler = function(event, context) {
-  console.log('Received event:', event)
+const handler = async function() {
+  console.log('Build hook triggered.')
+
+  await fetch('https://api.netlify.com/build_hooks/620bd20bc7433d2230c94653', {
+    method: 'POST'
+  }).then((response) => {
+    console.log('Build hook response:', response.json())
+  })
 
   return {
     statusCode: 200
   }
 }
 
-module.exports.handler = schedule('27 * * * *', handler)
+module.exports.handler = schedule('31 * * * *', handler)
