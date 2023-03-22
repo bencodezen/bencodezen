c966e71b09c7c484a428b52f7ec5e430323f82d4
Ben Hong <ben@bencodezen.io>
2023-03-21 23:56:18 -0700

    feat: add dynamic blog posts back in

---
+++ pages/blog/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { data: posts } = await useAsyncData('blog', () =>
+  queryContent('blog').find()
+)
+</script>
+
+<template>
+  <article class="post-section">
+    <div class="post-content">
+      <BlogPostList v-if="posts" :list="posts" />
+    </div>
+  </article>
+</template>
+
+<style></style>
