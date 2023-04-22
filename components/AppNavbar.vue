53b2caec6a3d3cabdb7847895c9663ebcaf8c399
Ben Hong <ben@bencodezen.io>
2023-04-21 20:04:42 -0400

    fix: remove schedule link

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -20,7 +20,6 @@ export default {}
     </div>
     <div class="navbar-item-wrapper">
       <nuxt-link to="/" class="navbar-item">Home</nuxt-link>
-      <nuxt-link to="/schedule" class="navbar-item">Schedule</nuxt-link>
       <nuxt-link to="/learn" class="navbar-item">Learn</nuxt-link>
       <nuxt-link to="/blog" class="navbar-item">Blog</nuxt-link>
       <nuxt-link to="/uses" class="navbar-item">Uses</nuxt-link>
