6e2bde4105fe02d8acfacbec301e8e8bd055769b
Ben Hong <ben@bencodezen.io>
2023-06-14 15:29:45 -0400

    docs: update ooh series

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -24,6 +24,24 @@ const eventList = ref<Event[]>([
     topic: 'First look at Omnivore',
     recording: {
       date: {
+        start: '2023-06-21T13:30:00-08:00',
+        end: '2023-06-21T14:30:00-08:00',
+      },
+      url: [
+        { label: 'Twitch', url: 'https://www.twitch.tv/bencodezen' },
+        {
+          label: 'YouTube',
+          url: 'https://www.youtube.com/bencodezen',
+        },
+      ],
+    },
+  },
+  {
+    id: '79a5c2c3-339a-4c95-a7e3-c1299faecba4',
+    series: 'Obsidian Office Hours',
+    topic: 'Update Night Owl Theme',
+    recording: {
+      date: {
         start: '2023-06-14T15:30:00-05:00',
         end: '2023-06-14T16:30:00-05:00',
       },
@@ -36,7 +54,6 @@ const eventList = ref<Event[]>([
       ],
     },
   },
-
   {
     id: '4288f6f8-ea22-4c4e-85b3-7b02bf2c74ba',
     series: 'Build with Ben',
