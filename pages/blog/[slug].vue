ebb2d569b4b904bafc14e6fe4d2243b1ce952ad1
Ben Hong <ben@bencodezen.io>
2023-06-20 14:43:12 -0700

    feat: user can see ghost content on blog site directly

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,27 +1,51 @@
 <script setup lang="ts">
 const route = useRoute()
-const { data: post } = await useAsyncData('blogPost', () =>
+
+const { data: localPost } = await useAsyncData('blogPost', () =>
   queryContent(`blog/${route.params.slug}`).findOne()
 )
 
-const formatPublishDate = computed(() => {
-  const dateFormat = new Date(post.value.date)
+const { data: ghostPost } = useFetch('/api/ghost', {
+  method: 'POST',
+  body: {
+    slug: route.params.slug,
+  },
+})
+
+if (localPost.value) {
+  console.log('Found it in Nuxt Content')
+} else {
+  console.log('It should be in Ghost!')
+}
+
+const formatPublishDate = (isoDateString) => {
+  const dateFormat = new Date(isoDateString)
   const options = {
     year: 'numeric',
     month: 'long',
-    day: 'numeric'
+    day: 'numeric',
   }
 
   return dateFormat.toLocaleDateString('en-US', options)
-})
+}
 </script>
 
 <template>
   <article class="post-section">
     <div class="post-content">
-      <p class="blog-publish-date">{{ formatPublishDate }}</p>
-      <h1 class="blog-title">{{ post.title }}</h1>
-      <ContentDoc :path="post._path" />
+      <template v-if="localPost">
+        <p class="blog-publish-date">{{ formatPublishDate }}</p>
+        <h1 class="blog-title">{{ localPost.title }}</h1>
+        <ContentDoc :path="localPost._path" />
+      </template>
+      <template v-else>
+        <p class="blog-publish-date">
+          {{ formatPublishDate(ghostPost.published_at) }}
+        </p>
+
+        <h1 class="blog-title">{{ ghostPost.title }}</h1>
+        <div v-html="ghostPost.html" />
+      </template>
     </div>
   </article>
 </template>
--- server/api/ghost.ts
+++ server/api/ghost.ts
@@ -1,11 +1,20 @@
 export default defineEventHandler(async (event) => {
-  const ghostContent = await $fetch(
-    'https://bencodezen.ghost.io/ghost/api/content/posts/?key=81bd665ebdb4dfcb238c38995b&include=tags'
-  )
+  if (event.req.method === 'POST') {
+    const { slug } = JSON.parse(event.req.body)
+    const ghostPost = await $fetch(
+      `https://bencodezen.ghost.io/ghost/api/content/posts/slug/${slug}?key=81bd665ebdb4dfcb238c38995b&include=tags`
+    )
 
-  const posts = ghostContent.posts.filter((post) => {
-    return post.tags.findIndex((tag) => tag.name === 'Post') !== -1
-  })
+    return ghostPost.posts[0]
+  } else {
+    const ghostContent = await $fetch(
+      'https://bencodezen.ghost.io/ghost/api/content/posts/?key=81bd665ebdb4dfcb238c38995b&include=tags'
+    )
 
-  return posts
+    const posts = ghostContent.posts.filter((post) => {
+      return post.tags.findIndex((tag) => tag.name === 'Post') !== -1
+    })
+
+    return posts
+  }
 })
